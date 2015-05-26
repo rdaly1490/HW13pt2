7c3e0208fa7a71d164a75f2a9f0b1d696e038ed4
Rob <rdaly1490@gmail.com>
2015-05-26 13:01:48 -0500

    loading button done

--- dist/main.js
+++ dist/main.js
@@ -2,11 +2,23 @@ $(document).on('ready', start);
 
 function start(eventObj) {
 
+$loading = $("#loadingBtn");
+$loading.on("click", disabledBtn);
 
+function disabledBtn(e) {
+	$loading.disabled = true;
+	$loading.html("Loading...");
+	setTimeout(enable, 4000);
+}
 
-$like.css("height", "50px");
-$like.css("width", "100px");
-$like.css("backgroundColor", "gray");
+function enable(e) {
+	$loading.disabled = false;
+	$loading.html("Submit");
+}
+
+$loading.css("height", "50px");
+$loading.css("width", "100px");
+$loading.css("backgroundColor", "gray");
 
 
 
