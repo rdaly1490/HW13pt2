29d689877ba824759ebf7724cbe9caf7cdab2cd6
Rob <rdaly1490@gmail.com>
2015-05-26 13:07:26 -0500

    minor color changes

--- dist/main.js
+++ dist/main.js
@@ -8,12 +8,16 @@ $loading.on("click", disabledBtn);
 function disabledBtn(e) {
 	$loading.disabled = true;
 	$loading.html("Loading...");
+	$loading.css("color", "#b7b7c2");
+	$loading.css("backgroundColor", "#dcdce1");
 	setTimeout(enable, 4000);
 }
 
 function enable(e) {
 	$loading.disabled = false;
 	$loading.html("Submit");
+	$loading.css("color", "black");
+	$loading.css("backgroundColor", "gray");
 }
 
 $loading.css("height", "50px");
