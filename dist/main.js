33a4f22c6dd8350918f34b210818a8c374f29747
Rob <rdaly1490@gmail.com>
2015-05-26 12:43:58 -0500

    fixed bower

---
+++ dist/main.js
@@ -0,0 +1,23 @@
+$(document).on('ready', start);
+
+function start(eventObj) {
+
+
+
+$like.css("height", "50px");
+$like.css("width", "100px");
+$like.css("backgroundColor", "gray");
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
